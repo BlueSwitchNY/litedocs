d172a272e55f7ed8c52a846cbe11790d780e6049
Perry Raskin <perryraskin@Perrys-MacBook-Air.local>
2020-12-22 15:12:57 -0500

    dotenv config

--- next.config.js
+++ next.config.js
@@ -2,9 +2,18 @@ require("dotenv").config()
 const dotenv = require("dotenv-webpack")
 
 module.exports = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.plugins.push(new dotenv({ silent: true }))
-
-    return config
+  serverRuntimeConfig: {
+    BASE_URL: process.env.BASE_URL,
+    ENCRYPTION_SECRET: process.env.ENCRYPTION_SECRET,
+    MAGIC_PUBLIC_KEY: process.env.MAGIC_PUBLIC_KEY,
+    MAGIC_SECRET_KEY: process.env.MAGIC_SECRET_KEY,
+    MAGIC_PUBLIC_KEY_LIVE: process.env.MAGIC_PUBLIC_KEY_LIVE,
+    MAGIC_SECRET_KEY_LIVE: process.env.MAGIC_SECRET_KEY_LIVE,
+    DATABASE_URL: process.env.DATABASE_URL,
   },
+  // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+  //   config.plugins.push(new dotenv({ silent: true }))
+
+  //   return config
+  // },
 }
