f1414693f054be1ef30d53912339cbd1d683fa27
Perry Raskin <perryraskin@gmail.com>
2020-12-23 15:13:01 -0500

    Adds temp favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,25 @@ class MyApp extends App {
     return (
       <>
         <Head>
+          <link
+            rel="icon"
+            type="image/x-icon"
+            sizes="16x16 32x32"
+            href="favicon.ico"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
